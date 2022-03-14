6ea335f43906ed6eb7fb0400b997a9d6d161d156
Remu Ollinen <remu.ollinen@gmail.com>
2022-03-14 09:44:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import * as actionTypes from "./components/store/actions";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const counter = useSelector((state) => state.counter);
+	const dispatch = useDispatch();
+
+	const addOneHandler = () => {
+		dispatch({
+			type: actionTypes.ADD_ONE,
+		});
+	};
+	const addFiveHandler = () => {
+		dispatch({
+			type: actionTypes.ADD_FIVE,
+		});
+	};
+	const removeOneHandler = () => {
+		if (counter > 0)
+			dispatch({
+				type: actionTypes.REMOVE_ONE,
+			});
+	};
+	const removeFiveHandler = () => {
+		if (counter > 4)
+			dispatch({
+				type: actionTypes.REMOVE_FIVE,
+			});
+	};
+	const resetHandler = () => {
+		dispatch({
+			type: actionTypes.RESET,
+		});
+	};
+
+	return (
+		<div className="App">
+			<header>Redux Counter App</header>
+			<main>
+				<div className={`number ${counter % 2 === 0 ? "even" : "odd"}`}>
+					{counter}
+				</div>
+				<div className="buttons">
+					<button onClick={addOneHandler}>Add one</button>
+					<button onClick={addFiveHandler}>Add five</button>
+					<button onClick={removeOneHandler}>Remove one</button>
+					<button onClick={removeFiveHandler}>Remove five</button>
+					<button onClick={resetHandler}>Reset</button>
+				</div>
+			</main>
+			<footer>&copy; Remu Ollinen 2022</footer>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/store/reducer.js
@@ -0,0 +1,24 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+	counter: 0,
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionTypes.ADD_ONE:
+			return { ...state, counter: state.counter + 1 };
+		case actionTypes.ADD_FIVE:
+			return { ...state, counter: state.counter + 5 };
+		case actionTypes.REMOVE_ONE:
+			return { ...state, counter: state.counter - 1 };
+		case actionTypes.REMOVE_FIVE:
+			return { ...state, counter: state.counter - 5 };
+		case actionTypes.RESET:
+			return { ...state, counter: 0 };
+		default:
+			return state;
+	}
+};
+
+export default reducer;
